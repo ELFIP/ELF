74a405d5708d9791f69385ca339f1649412162ee
Mihai Alexandru <mihaialexandru9@yahoo.ro>
2015-04-02 01:37:45 +0300

    Adaugare Administrare_utilizator

---
+++ SiteIP/Administrare_utilizator.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Administrare_utilizator.aspx.cs" Inherits="Administrare_utilizator" MasterPageFile="~/MasterPage.master"%>
+
+
+<asp:Content runat="server" ID="Content1" ContentPlaceHolderID="head">
+</asp:Content>
+<asp:Content runat="server" ID="content" ContentPlaceHolderID="ContentPlaceHolder1">
+    <div style="margin-left: auto; margin-right: auto; text-align: center;">
+        <asp:Label ID="label_cauta_utilizator" runat="server" Text="Cauta utilizator"></asp:Label>
+        <br />
+        <asp:TextBox ID="cauta_utilizator" runat="server" Style="margin:5px; padding: 5px;" Width="30%" OnTextChanged="Page_Load"></asp:TextBox>
+    </div>
+    <hr />
+    <div style="margin-left: auto; margin-right: auto; text-align: center;">
+        <asp:Table ID="tabel_utilizatori" runat="server" Width="75%" HorizontalAlign="Center">
+            <asp:TableHeaderRow runat="server">
+
+            </asp:TableHeaderRow>
+        </asp:Table>
+
+    </div>
+</asp:Content>
---
+++ SiteIP/Administrare_utilizator.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class Administrare_utilizator : System.Web.UI.Page
+{
+    public String[] email = new String[20];
+    public String[] nume = new String[20];
+    public String[] prenume = new String[20];
+    public String[] parola = new String[20];
+
+    int numar_utilizatori = 0;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        numar_utilizatori = 0;
+        SqlCommand comanda = new SqlCommand();
+        SqlConnection conexiune;
+        conexiune = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\Mihai Alexandru\Dropbox\Facultate\Anul 3\Semestrul II\Ingineria programarii\Site - repository\ELF\SiteIP\App_Data\Database.mdf';Integrated Security=True");
+        comanda.Connection = conexiune;
+        comanda.Connection.Open();
+        SqlDataReader sdr;
+        comanda.CommandText = "SELECT email, nume, prenume, parola FROM Utilizator WHERE email LIKE '%" + cauta_utilizator.Text + "%' ORDER BY data_inregistrarii;";
+        sdr = comanda.ExecuteReader();
+        while (sdr.Read())
+        {
+            email[numar_utilizatori] = sdr.GetValue(0).ToString();
+            nume[numar_utilizatori] = sdr.GetValue(1).ToString();
+            prenume[numar_utilizatori] = sdr.GetValue(2).ToString();
+            parola[numar_utilizatori] = sdr.GetValue(3).ToString();
+            numar_utilizatori++;
+        }
+        conexiune.Close();
+        afiseazaUtilizatori();
+    }
+
+    public void schimbaTabelul(object sender, EventArgs e)
+    {
+        numar_utilizatori = 0;
+        SqlCommand comanda = new SqlCommand();
+        SqlConnection conexiune;
+        conexiune = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\Mihai Alexandru\Dropbox\Facultate\Anul 3\Semestrul II\Ingineria programarii\Site - repository\ELF\SiteIP\App_Data\Database.mdf';Integrated Security=True");
+        comanda.Connection = conexiune;
+        comanda.Connection.Open();
+        SqlDataReader sdr;
+        comanda.CommandText = "SELECT email, nume, prenume, parola FROM Utilizator WHERE email LIKE '%" + cauta_utilizator.Text + "%' ORDER BY data_inregistrarii;";
+        sdr = comanda.ExecuteReader();
+        while (sdr.Read())
+        {
+            email[numar_utilizatori] = sdr.GetValue(0).ToString();
+            nume[numar_utilizatori] = sdr.GetValue(1).ToString();
+            prenume[numar_utilizatori] = sdr.GetValue(2).ToString();
+            parola[numar_utilizatori] = sdr.GetValue(3).ToString();
+            numar_utilizatori++;
+        }
+        conexiune.Close();
+        afiseazaUtilizatori();
+    }
+
+    public void afiseazaUtilizatori()
+    {
+        tabel_utilizatori.Rows.Clear();
+       
+        TableHeaderRow header = new TableHeaderRow();
+        header.Width = new Unit("75%");
+        TableHeaderCell header_celula1 = new TableHeaderCell();
+        header_celula1.Text = "Email";
+        header_celula1.Width = new Unit("30%");
+        TableHeaderCell header_celula2 = new TableHeaderCell();
+        header_celula2.Text = "Nume";
+        header_celula2.Width = new Unit("30%");
+        TableHeaderCell header_celula3 = new TableHeaderCell();
+        header_celula3.Text = "Prenume";
+        header_celula3.Width = new Unit("30%");
+
+        header.Controls.Add(header_celula1);
+        header.Controls.Add(header_celula2);
+        header.Controls.Add(header_celula3);
+
+        tabel_utilizatori.Controls.Add(header);
+
+        for (int i = 0; i < numar_utilizatori; i ++ )
+        {
+            TableCell celula1 = new TableCell();
+            celula1.Text = email[i];
+            TableCell celula2 = new TableCell();
+            celula2.Text = nume[i];
+            TableCell celula3 = new TableCell();
+            celula3.Text = prenume[i];
+            TableRow rand = new TableRow();
+            rand.Controls.Add(celula1);
+            rand.Controls.Add(celula2);
+            rand.Controls.Add(celula3);
+            tabel_utilizatori.Controls.Add(rand);
+        }
+    }
+
+}
