0daef1d587119fb4480d3b07ad573a8936a05c7f
Mihai Alexandru <mihaialexandru9@yahoo.ro>
2015-04-02 21:51:27 +0300

    Adaugare string baza de date

---
+++ SiteIP/App_Code/Auxiliare.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Auxiliare
+/// </summary>
+public class Auxiliare
+{
+    public String string_bazadedate =  @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\Mihai Alexandru\Dropbox\Facultate\Anul 3\Semestrul II\Ingineria programarii\Site - repository\ELF1\ELF\SiteIP\App_Data\Database.mdf';Integrated Security=True" ;
+
+}
