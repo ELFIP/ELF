3a7b1d964bda26f1249e275823d1224303a00b21
ovmihai <valentinovi93@yahoo.com>
2015-04-04 16:23:08 +0300

    Login page

---
+++ SiteIP/App_Code/DateUtilizator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DateUtilizator
+/// </summary>
+public class DateUtilizator
+{
+    public static string nume;
+    public static string prenume;
+    public static string email;
+    public static bool administrator;
+	public DateUtilizator()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
---
+++ SiteIP/login.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class login : System.Web.UI.Page
+{
+    Auxiliare a = new Auxiliare();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btn_login_Click(object sender, EventArgs e)
+    {
+        if (email_valid(tb_email.Text))
+        {
+            if (tb_parola.Text.Equals(verificare_parola(tb_email.Text)))
+            {
+                DateUtilizator.email = tb_email.Text;
+                lbl_alertDateLogin.Text = "";
+                Response.Redirect("index.aspx");
+            }
+            else
+            {
+                lbl_alertDateLogin.Text = "Date incorecte !";
+            }
+        }
+        else
+        {
+            lbl_alertDateLogin.Text = "Date incorecte !";
+        }
+    }
+
+    private bool email_valid(string email)
+    {
+        SqlCommand comanda = new SqlCommand();
+        SqlConnection conexiune;
+        conexiune = new SqlConnection(a.string_bazadedate);
+        comanda.Connection = conexiune;
+        comanda.Connection.Open();
+        SqlDataReader sdr;
+        comanda.CommandText = "SELECT email FROM [Utilizator] where email = '" + email + "';";
+        sdr = comanda.ExecuteReader();
+        int i = 0;
+        while (sdr.Read())
+        {
+            i++;
+        }
+        conexiune.Close();
+        if (i == 0)
+        {
+            return false; // email-ul nu exista
+        }
+        else
+        {
+            return true; // emailul exista, verificam parola
+        }
+    }
+    private string verificare_parola(string email)
+    {
+        SqlCommand comanda = new SqlCommand();
+        SqlConnection conexiune;
+        conexiune = new SqlConnection(a.string_bazadedate);
+        comanda.Connection = conexiune;
+        comanda.Connection.Open();
+        SqlDataReader sdr;
+        comanda.CommandText = "SELECT parola FROM [Utilizator] where email = '" + email + "' ;";
+        sdr = comanda.ExecuteReader();
+        sdr.Read();
+        string password = sdr.GetValue(0).ToString();
+        conexiune.Close();
+        return password;
+    }
+    private string date_utlizator(string email)
+    {
+        SqlCommand comanda = new SqlCommand();
+        SqlConnection conexiune;
+        conexiune = new SqlConnection(a.string_bazadedate);
+        comanda.Connection = conexiune;
+        comanda.Connection.Open();
+        SqlDataReader sdr;
+        comanda.CommandText = "SELECT nume,prenume FROM [Utilizator] where email = '" + email + "' ;";
+        sdr = comanda.ExecuteReader();
+        sdr.Read();
+        string date = sdr.GetValue(0).ToString();
+        conexiune.Close();
+        return date;
+    }
+}
