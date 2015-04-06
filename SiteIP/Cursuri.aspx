a0c1bc0a01e7b5f243c2b438aa44def9d79a45d1
MerfuAlexandru <rila_alexx@yahoo.com>
2015-04-06 12:10:53 +0300

    Adaugare pagina cursuri

---
+++ SiteIP/Cursuri.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Cursuri.aspx.cs" Inherits="Cursuri" MasterPageFile="~/MasterPage.master" %>
+
+
+<asp:Content runat="server" ID="Content1" ContentPlaceHolderID="head">
+</asp:Content>
+<asp:Content runat="server" ID="content" ContentPlaceHolderID="ContentPlaceHolder1">
+  <div style="margin-left: auto; margin-right: auto; text-align: center;">
+        <asp:Label ID="label_cauta_utilizator" runat="server" Text="Cauta cursuri"></asp:Label>
+      <br />
+        <asp:TextBox ID="cauta_curs" runat="server" OnTextChanged="TextBox1_TextChanged" style="margin-bottom: 0px"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Cauta Curs" />
+    
+  </div>
+  <div>
+        <asp:Panel ID="cursuri" runat="server">
+
+        </asp:Panel>
+  </div>
+</asp:Content>
+
---
+++ SiteIP/Cursuri.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class Cursuri : System.Web.UI.Page
+{
+    public String[] nume = new String[20];
+    public float nota;
+    Auxiliare a = new Auxiliare();
+
+    int numar_cursuri = 0;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        numar_cursuri = 0;
+        SqlCommand comanda = new SqlCommand();
+        SqlConnection conexiune;
+        conexiune = new SqlConnection(a.string_bazadedate);
+        comanda.Connection = conexiune;
+        comanda.Connection.Open();
+        SqlDataReader sdr;
+        comanda.CommandText = "SELECT nume, nota, FROM Curs WHERE nume LIKE '%" + cauta_curs.Text + "%' ;";
+        sdr = comanda.ExecuteReader();
+        while (sdr.Read())
+        {
+            nume[numar_cursuri] = sdr.GetValue(0).ToString();
+            nota = int.Parse(sdr.GetValue(1).ToString());
+            numar_cursuri++;
+        }
+        conexiune.Close();
+        afiseazaCursuri();
+    }
+    
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    public void afiseazaCursuri()
+    {
+        
+        numar_cursuri = 0;
+        SqlCommand comanda = new SqlCommand();
+        SqlConnection conexiune;
+        conexiune = new SqlConnection(a.string_bazadedate);
+        comanda.Connection = conexiune;
+        comanda.Connection.Open();
+        SqlDataReader sdr;
+        comanda.CommandText = "SELECT nume,nota FROM Curs ;";
+        sdr = comanda.ExecuteReader();
+        while (sdr.Read())
+        {
+            Panel cursuri = new Panel();
+            nume[numar_cursuri] = sdr.GetValue(0).ToString();
+            nota = int.Parse(sdr.GetValue(1).ToString());
+            cursuri.Controls.Add(nume);
+            numar_cursuri++;
+        }
+        conexiune.Close();
+    }
+}
