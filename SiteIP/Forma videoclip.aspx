8d991744e0014a40ff19e3d28317316610ef650b
Mihai Alexandru <mihaialexandru9@yahoo.ro>
2015-05-08 12:05:17 +0300

    Actualizare Formular Videoclip

--- SiteIP/Forma videoclip.aspx
+++ SiteIP/Forma videoclip.aspx
@@ -6,18 +6,22 @@
 <asp:Content runat="server" ID="content" ContentPlaceHolderID="ContentPlaceHolder1">
        <asp:Table ID="formular" runat="server" Width="80%" style="margin-left: 10%">
             <asp:TableHeaderRow Width="100%" >
-                <asp:TableHeaderCell HorizontalAlign="Center" style="padding:10px">
-                    <asp:Label ID="numeVideoclip" runat="server" placeholder="Nume videoclip" Width="50%">
+                <asp:TableHeaderCell HorizontalAlign="Center" style="padding:10px" Width="50%">
+                    <asp:Label ID="numeVideoclip" runat="server" Text="" Width="50%">
+                    </asp:Label>
+                </asp:TableHeaderCell>
+                <asp:TableHeaderCell HorizontalAlign="Center" style="padding:10px" Width="50%">
+                    <asp:Label ID="numeCurs" runat="server" Text="" Width="50%">
                     </asp:Label>
                 </asp:TableHeaderCell>
             </asp:TableHeaderRow>
            <asp:TableRow>
-               <asp:TableCell HorizontalAlign="Center">
+               <asp:TableCell HorizontalAlign="Center" ColumnSpan="2">
                    <iframe ID="afiseaza" width="560" height="315" Src="" frameborder="0" allowfullscreen runat="server"></iframe>
                </asp:TableCell>
            </asp:TableRow>
            <asp:TableRow>
-               <asp:TableCell HorizontalAlign="Center">
+               <asp:TableCell HorizontalAlign="Center" ColumnSpan="2">
                    <asp:RadioButtonList ID="RadioButtonList1" runat="server" HorizontalAlign="Center" AutoPostBack ="true" OnSelectedIndexChanged="selectare">
                        <asp:ListItem ID="id_nota1" runat="server" Text="1">
 
--- SiteIP/Forma videoclip.aspx.cs
+++ SiteIP/Forma videoclip.aspx.cs
@@ -11,7 +11,9 @@ public partial class Forma_videoclip : System.Web.UI.Page
     Auxiliare a = new Auxiliare();
     int id_videoclip;
     int id_utilizator;
+    int id_curs;
     int nota_data;
+    double media_notelor_videoclip;
     private string email;
 
     protected void Page_Load(object sender, EventArgs e)
@@ -20,6 +22,7 @@ public partial class Forma_videoclip : System.Web.UI.Page
             culegeDate();
             selecteazaIdUtilizator();
             selecteazaIdVideoclip();
+            selecteazaIdCurs();
             selecteazaNotaDataVideoclip();
             afiseazaNotaData();
         }
@@ -67,6 +70,21 @@ public partial class Forma_videoclip : System.Web.UI.Page
         conexiune.Close();
     }
 
+    private void selecteazaIdCurs()
+    {
+        SqlCommand comanda = new SqlCommand();
+        SqlConnection conexiune;
+        conexiune = new SqlConnection(a.string_bazadedate);
+        comanda.Connection = conexiune;
+        comanda.Connection.Open();
+        SqlDataReader sdr;
+        comanda.CommandText = "SELECT id_curs FROM Curs WHERE nume = '" + numeCurs.Text + "';";
+        sdr = comanda.ExecuteReader();
+        sdr.Read();
+        id_curs = int.Parse(sdr.GetValue(0).ToString());
+        conexiune.Close();
+    }
+
     private void selecteazaIdVideoclip()
     {
         SqlCommand comanda = new SqlCommand();
@@ -110,18 +128,56 @@ public partial class Forma_videoclip : System.Web.UI.Page
         conexiune.Close();
     }
 
+    private void selecteazaMediaNotelorVideoclip()
+    {
+        SqlCommand comanda = new SqlCommand();
+        SqlConnection conexiune;
+        conexiune = new SqlConnection(a.string_bazadedate);
+        comanda.Connection = conexiune;
+        comanda.Connection.Open();
+        SqlDataReader sdr;
+        comanda.CommandText = "SELECT AVG(nota_data) FROM Utilizator_Videoclip WHERE id_videoclip = " + id_videoclip + ";";
+        sdr = comanda.ExecuteReader();
+        sdr.Read();
+        media_notelor_videoclip = int.Parse(sdr.GetValue(0).ToString());
+        conexiune.Close();
+    }
+
+    private void actualizeazaMediaNotelorVideoclip(int nota)
+    {
+        SqlCommand comanda = new SqlCommand();
+        SqlConnection conexiune;
+        conexiune = new SqlConnection(a.string_bazadedate);
+        comanda = new SqlCommand();
+        comanda.Connection = conexiune;
+        comanda.Connection.Open();
+        comanda.CommandText = "UPDATE [Videoclip] SET media_notelor = " + media_notelor_videoclip + " WHERE id_videoclip = " + id_videoclip + " AND id_curs = " + id_curs + ";";
+        comanda.ExecuteNonQuery();
+        conexiune.Close();
+    }
+
     protected void selectare(object sender, EventArgs e)
     {
         if(id_nota1.Selected == true) {
             actualizeazaNotaDataVideoclip(1);
+            selecteazaMediaNotelorVideoclip();
+            actualizeazaMediaNotelorVideoclip(1);
         } else if(id_nota2.Selected == true) {
             actualizeazaNotaDataVideoclip(2);
+            selecteazaMediaNotelorVideoclip();
+            actualizeazaMediaNotelorVideoclip(2);
         } else if(id_nota3.Selected == true) {
             actualizeazaNotaDataVideoclip(3);
+            selecteazaMediaNotelorVideoclip();
+            actualizeazaMediaNotelorVideoclip(3);
         } else if (id_nota4.Selected == true) {
             actualizeazaNotaDataVideoclip(4);
+            selecteazaMediaNotelorVideoclip();
+            actualizeazaMediaNotelorVideoclip(4);
         } else {
             actualizeazaNotaDataVideoclip(5);
+            selecteazaMediaNotelorVideoclip();
+            actualizeazaMediaNotelorVideoclip(5);
         }
     }
 
